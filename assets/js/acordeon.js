e1a737af147f7d172254bae9b705419f703a99ad
Davi Goncalves Friggi <dgfriggi@inovasaudesa.com.br>
2025-11-05 13:08:20 -0300

    acordeon finished

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,6 +1,23 @@
 const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
 
+const acordeonTriggersIn = document.querySelectorAll('.acordeon-vida .trigger-vida')
+
 acordeonTriggers.forEach((trigger) => {
+
+    trigger.addEventListener('click', (e) => {
+        const acordeon = trigger.parentElement
+
+        const isOpen = acordeon.classList.contains('open')
+
+        if(isOpen) {
+            acordeon.classList.remove('open')
+        } else {
+            acordeon.classList.add('open')
+        }
+    })
+})
+
+acordeonTriggersIn.forEach((trigger) => {
 
     trigger.addEventListener('click', (e) => {
         const acordeon = trigger.parentElement
